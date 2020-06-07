8d31e037ad006b11665217e787b8de42a6d70103
Luca Mefisto <luca.m.conesa@gmail.com>
2020-06-07 20:08:32 +0200

    commit with basic example

---
+++ HandPosing_Unity/Hand Posing/Assets/PoseAuthoring/Scripts/HandPuppet.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using static OVRSkeleton;
+
+namespace PoseAuthoring
+{
+    public class HandPuppet : MonoBehaviour
+    {
+        [SerializeField]
+        private OVRSkeleton trackedHand;
+
+        [SerializeField]
+        private List<BoneMap> boneMaps;
+
+        private Dictionary<BoneId, BoneMap> bonesCollection;
+
+
+        private void Awake()
+        {
+            InitializeBones();
+        }
+
+
+        private void InitializeBones()
+        {
+            bonesCollection = new Dictionary<BoneId, BoneMap>();
+            foreach (var boneMap in boneMaps)
+            {
+                BoneId id = boneMap.id;
+                bonesCollection.Add(id, boneMap);
+            }
+        }
+
+
+        private void LateUpdate()
+        {
+            if (trackedHand.IsInitialized 
+                && trackedHand.IsDataValid)
+            {
+                SetPose(trackedHand);
+            }
+        }
+
+
+        private void SetPose(OVRSkeleton skeleton)
+        {
+            for (int i = 0; i < skeleton.Bones.Count; ++i)
+            {
+                BoneId boneId = (BoneId)skeleton.Bones[i].Id;
+                if (bonesCollection.ContainsKey(boneId))
+                {
+                    Transform boneTransform = bonesCollection[boneId].transform;
+                    Quaternion offset = Quaternion.Euler(bonesCollection[boneId].rotationOffset);
+                    Quaternion desiredRot = skeleton.Bones[i].Transform.localRotation;
+                    boneTransform.localRotation = offset * desiredRot;
+                }
+            }
+        }
+    }
+
+    [System.Serializable]
+    public class BoneMap
+    {
+        public BoneId id;
+        public Transform transform;
+        public Vector3 rotationOffset;
+    }
+}
---
+++ HandPosing_Unity/Hand Posing/Assets/PoseAuthoring/Scripts/SkeletonAxisDrawer.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace PoseAuthoring
+{
+    public class SkeletonAxisDrawer : MonoBehaviour
+    {
+        [SerializeField]
+        private OVRSkeleton skeleton;
+        [SerializeField]
+        private Transform axisPrototype;
+
+        private Transform[] axises;
+
+
+        private void InitializeAxis(OVRSkeleton skeleton)
+        {
+            axises = new Transform[skeleton.Bones.Count];
+            for (int i = 0; i < skeleton.Bones.Count; i++)
+            {
+                axises[i] = Instantiate<Transform>(axisPrototype, this.transform);
+            }
+        }
+
+
+        void Update()
+        {
+            if (skeleton.IsInitialized && skeleton.IsDataValid)
+            {
+                if(axises == null)
+                {
+                    InitializeAxis(skeleton);
+                }
+
+                for (int i = 0; i < skeleton.Bones.Count; i++)
+                {
+                    axises[i].SetPositionAndRotation(skeleton.Bones[i].Transform.position, skeleton.Bones[i].Transform.rotation);
+                }
+            }
+            
+        }
+    }
+}
