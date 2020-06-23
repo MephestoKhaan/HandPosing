65e8eef62e57ad080129a8e09a37b735bbc9ae1b
Luca Mefisto <luca.m.conesa@gmail.com>
2020-06-23 10:49:00 +0200

    started making my own cylinder editor

---
+++ HandPosing_Unity/Hand Posing/Assets/PoseAuthoring/Scripts/CylinderHandle.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+
+public class CylinderHandle : MonoBehaviour
+{
+    public Vector3 StartPoint
+    {
+        get
+        {
+            return this.transform.position + Vector3.up * 0.5f;
+        }
+    }
+    public Vector3 EndPoint
+    {
+        get
+        {
+            return this.transform.position - Vector3.up * 0.5f;
+        }
+    }
+
+    public Vector3 Tangent
+    {
+        get
+        {
+            return Vector3.Cross(Direction, Vector3.forward).normalized;
+        }
+    }
+    public float angle = 270f;
+    public float radious;
+
+    public Vector3 Direction
+    {
+        get
+        {
+            Vector3 dir = (EndPoint - StartPoint);
+            if(dir.sqrMagnitude == 0f)
+            {
+                return this.transform.up;
+            }
+            return dir.normalized;
+        }
+    }
+
+    
+
+}
---
+++ HandPosing_Unity/Hand Posing/Assets/PoseAuthoring/Scripts/Editor/CylinderHandleEditor.cs
@@ -0,0 +1,59 @@
+﻿using UnityEditor;
+using UnityEditor.IMGUI.Controls;
+using UnityEngine;
+
+[CustomEditor(typeof(CylinderHandle))]
+[CanEditMultipleObjects]
+public class CylinderHandleEditor : Editor
+{
+    private static readonly Color NONINTERACTABLE_COLOR = new Color(0f, 1f, 1f, 0.3f);
+    private static readonly Color INTERACTABLE_COLOR = new Color(0f, 1f, 1f, 1f);
+
+
+    //ArcHandle arcHandle = new ArcHandle();
+
+    public void OnSceneGUI()
+    {
+        if (Event.current.type != EventType.Repaint)
+        {
+           // return;
+        }
+        CylinderHandle cylinder = (target as CylinderHandle);
+
+        EditorGUI.BeginChangeCheck();
+
+        Handles.color = NONINTERACTABLE_COLOR;
+        Handles.DrawWireDisc(cylinder.StartPoint, cylinder.Direction, cylinder.radious);
+        Handles.DrawSolidArc(cylinder.StartPoint, cylinder.Direction, cylinder.Tangent, cylinder.angle, cylinder.radious);
+
+
+
+
+        Handles.color = INTERACTABLE_COLOR;
+        Vector3 startAnglePosition = cylinder.StartPoint + cylinder.Tangent * cylinder.radious;
+        Handles.CylinderHandleCap(
+            0,
+            startAnglePosition,
+            Quaternion.identity,
+            HandleUtility.GetHandleSize(startAnglePosition) * 0.25f,
+            EventType.Repaint
+        );
+
+        Vector3 endAnglePosition = cylinder.StartPoint + Quaternion.AngleAxis(cylinder.angle, cylinder.Direction) * (cylinder.Tangent * cylinder.radious);
+        Handles.CylinderHandleCap(
+            0,
+            endAnglePosition,
+            Quaternion.identity,
+            HandleUtility.GetHandleSize(endAnglePosition) * 0.25f,
+            EventType.Repaint
+        );
+
+
+
+        if (EditorGUI.EndChangeCheck())
+        {
+            Undo.RecordObject(target, "Cylinder Rotate");
+
+        }
+    }
+}
