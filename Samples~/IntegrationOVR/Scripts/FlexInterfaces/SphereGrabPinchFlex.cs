06eed5805318cd57a35b8c3426b1a8932ca12ac7
LucaMefisto <luca.m.conesa@gmail.com>
2021-03-11 11:37:12 +0100

    better decision between Sphere and Pinch grabs using hysteresis

--- Samples~/IntegrationOVR/Scripts/FlexInterfaces/SphereGrabPinchFlex.cs
+++ Samples~/IntegrationOVR/Scripts/FlexInterfaces/SphereGrabPinchFlex.cs
@@ -87,17 +87,18 @@ namespace HandPosing.OVRIntegration.GrabEngine
         public float? CalculateGrabStrength()
         {
             _lastWasSphereFlex = false;
-            float pinchStrength = pinchFlex.GrabStrength ?? -1f;
+
+            float pinchStrength = RemapedFlex(pinchFlex, out float rawPinchStrength) ?? -1f;
             if (pinchStrength == 1f)
             {
                 return 1f;
             }
 
-            float sphereStrength = sphereFlex.GrabStrength ?? -1f;
+            float sphereStrength = RemapedFlex(sphereFlex, out float rawSphereStrength) ?? -1f;
             if (sphereStrength > pinchStrength)
             {
                 _lastWasSphereFlex = true;
-                return sphereStrength;
+                return rawSphereStrength;
             }
 
             if (pinchStrength == -1f)
@@ -105,7 +106,25 @@ namespace HandPosing.OVRIntegration.GrabEngine
                 return null;
             }
 
-            return pinchStrength;
+            return rawPinchStrength;
+        }
+
+        private static float? RemapedFlex(FlexInterface flex, out float rawStrenght)
+        {
+            float? strenght = flex.GrabStrength;
+            rawStrenght = strenght ?? -1f;
+            if (!strenght.HasValue)
+            {
+                return null;
+            }
+            Vector2 range = flex.GrabThresold;
+            return RemapClamped(strenght.Value, range.x, range.y, 0f, 1f);
+        }
+
+        private static float RemapClamped(float value, float low1, float high1, float low2 = 0f, float high2 = 1f)
+        {
+            value = low1 < high1 ? Mathf.Clamp(value, low1, high1) : Mathf.Clamp(value, high1, low1);
+            return low2 + (value - low1) * (high2 - low2) / (high1 - low1);
         }
     }
 }
