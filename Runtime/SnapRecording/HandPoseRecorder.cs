315b19b0842bff7f0716b6424303b703eb240396
LucaMefisto <luca.m.conesa@gmail.com>
2021-05-25 13:32:57 +0200

    recorded using proper recording keys and removed distance grab from demos

--- Runtime/SnapRecording/HandPoseRecorder.cs
+++ Runtime/SnapRecording/HandPoseRecorder.cs
@@ -23,12 +23,20 @@ namespace HandPosing.SnapRecording
         [SerializeField]
         [Tooltip("Must implement IGrabNotifier")]
         private Component grabber;
+
+#if UNITY_INPUTSYSTEM
+        [SerializeField]
+        private string recordKey = "space";
+#else
+        [SerializeField]
+        private KeyCode recordKeyCode = KeyCode.Space;
+#endif
+
         /// <summary>
         /// Create an Inspector button for manually triggering the pose recorer.
         /// </summary>
         [InspectorButton("RecordPose")]
         public string record;
-
 
         private IGrabNotifier _grabNotifier;
 
@@ -47,14 +55,14 @@ namespace HandPosing.SnapRecording
 #if UNITY_INPUTSYSTEM
         private void Start()
         {
-            UnityEngine.InputSystem.InputAction recordAction = new UnityEngine.InputSystem.InputAction("onRecordPose", binding: "<Keyboard>/space");
+            UnityEngine.InputSystem.InputAction recordAction = new UnityEngine.InputSystem.InputAction("onRecordPose", binding: $"<Keyboard>/{recordKey}");
             recordAction.Enable();
             recordAction.performed += ctx => RecordPose();
         }
 #else
         private void Update()
         {
-            if(Input.GetKeyDown(KeyCode.Space))
+            if(Input.GetKeyDown(recordKeyCode))
             {
                 RecordPose();
             }
